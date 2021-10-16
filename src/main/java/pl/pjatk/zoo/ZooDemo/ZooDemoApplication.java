3f7951864d61a40b31677ab93f998b07236fb55b
Mateusz Anikiej <mateusz.anikiej@gmail.com>
2021-10-16 18:10:26 +0200

    initial commit

---
+++ src/main/java/pl/pjatk/zoo/ZooDemo/ZooDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.zoo.ZooDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/zoo/ZooDemo/ZooDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.zoo.ZooDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
